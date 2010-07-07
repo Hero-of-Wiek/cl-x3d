84e9d621fa4088b6a9afc7f6e7e32505fd83aed6
James S <i@nixeagle.org>
2010-07-07 01:51:35 +0000

    Add SF-BOOL-P

--- src/x3d.lisp
+++ src/x3d.lisp
@@ -1,3 +1,6 @@
 (in-package :x3d.types)
 
-
+(defun sf-bool-p (input)
+  "True if INPUT contains true or false as a string."
+  (when (stringp input)
+    (member input '("True" "False" "") :test #'equalp)))
