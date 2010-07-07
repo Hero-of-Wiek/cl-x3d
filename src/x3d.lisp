51d80281a13647ec9d593f50617398b81850c7f6
James S <i@nixeagle.org>
2010-07-07 03:34:55 +0000

    Define PARSE-POINTS and PARSE-SF-BOOL

--- src/x3d.lisp
+++ src/x3d.lisp
@@ -4,3 +4,25 @@
   "True if INPUT contains true or false as a string."
   (when (stringp input)
     (member input '("True" "False" "") :test #'equalp)))
+
+
+(in-package :x3d.parse)
+
+(defun parse-sf-bool (string)
+  (declare (string string))
+  (check-type string (satisfies sf-bool-p))
+  (equalp "True" string))
+
+
+(defun parse-points (string)
+  "Parse a list of .x3d points into a list of vectors.
+
+Our result is something like (list #(1 2 3) #(3.4 3.4 4.5))"
+  (declare (string string))
+  (mapcar (lambda (vect)
+            (mapcar #'read-from-string
+                    (split-sequence:split-sequence
+                     #\Space vect :remove-empty-subseqs t)))
+          (remove " "
+                  (split-sequence:split-sequence #\, string :remove-empty-subseqs t)
+                  :test #'string=)))
